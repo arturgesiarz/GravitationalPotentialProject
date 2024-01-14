c6ff21c5040e1573165914bf71faf20d7bbd7332
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2024-01-14 23:17:43 +0100

    Integral was implemented

--- src/main/java/gravitationalPotential/model/Integral.java
+++ src/main/java/gravitationalPotential/model/Integral.java
@@ -1,4 +1,19 @@
 package gravitationalPotential.model;
-
+import java.util.function.DoubleUnaryOperator;
 public class Integral {
+    public static double calculateIntegral(DoubleUnaryOperator function, double lowerBand, double upperBand){
+        double result = 0;
+        int intervals = 10000;
+        double h = (upperBand - lowerBand) / intervals;
+
+        double last = lowerBand;
+        double next = lowerBand + h;
+
+        for(int i = 0; i < intervals; i++){
+            result += ( ( next - last ) / 2 * (function.applyAsDouble(last) + function.applyAsDouble(next) ) );
+            last = next;
+            next += h;
+        }
+        return result;
+    }
 }
