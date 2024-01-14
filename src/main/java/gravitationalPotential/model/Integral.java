f7104c776f3af776bdabcd4200089479e212dc0b
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2024-01-14 23:22:31 +0100

    Simpson's method was implemented

--- src/main/java/gravitationalPotential/model/Integral.java
+++ src/main/java/gravitationalPotential/model/Integral.java
@@ -1,6 +1,13 @@
 package gravitationalPotential.model;
 import java.util.function.DoubleUnaryOperator;
+
 public class Integral {
+
+    /**
+     * The quadrature integral is
+     * calculated using Simpson's method.
+     *
+     */
     public static double calculateIntegral(DoubleUnaryOperator function, double lowerBand, double upperBand){
         double result = 0;
         int intervals = 10000;
@@ -8,11 +15,14 @@ public class Integral {
 
         double last = lowerBand;
         double next = lowerBand + h;
+        double middle = (last + next) / 2;
 
         for(int i = 0; i < intervals; i++){
-            result += ( ( next - last ) / 2 * (function.applyAsDouble(last) + function.applyAsDouble(next) ) );
+            result += ( ( next - last ) / 6 *
+                    ( function.applyAsDouble(last) + 4 * function.applyAsDouble(middle) + function.applyAsDouble(next) ) );
             last = next;
             next += h;
+            middle = ( last + next ) / 2;
         }
         return result;
     }
