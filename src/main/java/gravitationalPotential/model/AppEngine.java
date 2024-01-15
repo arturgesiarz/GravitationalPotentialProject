a48fa62e6395d55780c77395247d17a2cb330a9b
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2024-01-15 11:19:14 +0100

    App

--- src/main/java/gravitationalPotential/model/AppEngine.java
+++ src/main/java/gravitationalPotential/model/AppEngine.java
@@ -5,6 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
+import java.io.IOException;
+import java.net.URL;
 
 
 public class AppEngine extends Application {
@@ -16,11 +20,20 @@ public class AppEngine extends Application {
         configureStage(primaryStage, viewRoot);
         primaryStage.show();
     }
-    private void configureStage(Stage primaryStage, BorderPane viewRoot) {
+    private void configureStage(Stage primaryStage, BorderPane viewRoot){
         Scene scene = new Scene(viewRoot);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Gravitation Potential app");
         primaryStage.minWidthProperty().bind(viewRoot.minWidthProperty());
         primaryStage.minHeightProperty().bind(viewRoot.minHeightProperty());
+
+        try{
+            URL location  = getClass().getClassLoader().getResource("icon.png");
+            if(location != null) {
+                primaryStage.getIcons().add(new Image(location.openStream()));
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
